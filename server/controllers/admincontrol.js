fe20f17e35d76acfda9c81b590bde32f03d704e1
Abhin prmod <abhinabhi310@gmail.com>
2025-02-15 01:15:49 +0530

    contractor block and unblock seted

--- server/controllers/admincontrol.js
+++ server/controllers/admincontrol.js
@@ -1,106 +1,103 @@
-const  Admin = require ("../model/admin.js");
+const Admin = require("../model/admin.js");
 const bcrypt = require("bcrypt");
-const   generateToken =require ("../lib/utils.js" )
-const sendEmail =require ("../lib/mailer.js" )
+const generateToken = require("../lib/utils.js");
+const sendEmail = require("../lib/mailer.js");
 
 const addadmin = async (req, res) => {
-  const { email, password, fullname, role ,uniqueId} = req.body;
-  console.log(req.body);  
-  
+  const { email, password, fullname, role, uniqueId } = req.body;
+  console.log(req.body);
 
   try {
-      if (!fullname || !email || !password ) {
-          return res.status(400).json({ message: "All fields are required man" });
-      }
+    if (!fullname || !email || !password) {
+      return res.status(400).json({ message: "All fields are required man" });
+    }
 
-      if (password.length < 6) {
-          return res
-              .status(400)
-              .json({ message: "Password must be at least 6 characters" });
-      }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
 
-      const user = await Admin.findOne({ email });
-      if (user) return res.status(400).json({ msg: "Admin already exists" });
+    const user = await Admin.findOne({ email });
+    if (user) return res.status(400).json({ msg: "Admin already exists" });
 
-      const salt = await bcrypt.genSalt();
-      const hashPassword = await bcrypt.hash(password, salt);
-      
-      const newadmin = new Admin({
-          fullname,
-          email,
-          password: hashPassword,
-          role,
-          isBlocked: false,
-          uniqueId: uniqueId
-      });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
 
-      await newadmin.save(); // Save the admin
-      const subject = 'Welcome to Our Application';
-      const text = `Hello ${fullname},\n\nYou have been added as an admin to our application.\n\nPlease use the following credentials to log in:\n\nEmail: ${email}\nPassword: ${password}\n\nIf you have any questions, please contact our support team.\n\nThank you,\nOur Team`;
-  
-      sendEmail(email, subject, text);
-      console.log("Email sent:", email, subject, text);
+    const newadmin = new Admin({
+      fullname,
+      email,
+      password: hashPassword,
+      role,
+      isBlocked: false,
+      uniqueId: uniqueId,
+    });
 
-      return res.status(201).json({
-          _id: newadmin._id,
-          fullname: newadmin.fullname,
-          email: newadmin.email,
-          role: newadmin.role,
-          uniqueId: newadmin.uniqueId
-      });
+    await newadmin.save(); // Save the admin
+    const subject = "Welcome to Our Application";
+    const text = `Hello ${fullname},\n\nYou have been added as an admin to our application.\n\nPlease use the following credentials to log in:\n\nEmail: ${email}\nPassword: ${password}\n\nIf you have any questions, please contact our support team.\n\nThank you,\nOur Team`;
 
+    sendEmail(email, subject, text);
+    console.log("Email sent:", email, subject, text);
+
+    return res.status(201).json({
+      _id: newadmin._id,
+      fullname: newadmin.fullname,
+      email: newadmin.email,
+      role: newadmin.role,
+      uniqueId: newadmin.uniqueId,
+    });
   } catch (error) {
-      console.log("Error from addadmin:", error.message);
-      res.status(500).json({ msg: "Internal server error" });
+    console.log("Error from addadmin:", error.message);
+    res.status(500).json({ msg: "Internal server error" });
   }
 };
 const login = async (req, res) => {
-const { email, password } = req.body;
+  const { email, password } = req.body;
 
-try {
-  const admin = await Admin.findOne({ email });
-  if (!admin) return res.status(400).json({ msg: "Invalid Email" });
+  try {
+    const admin = await Admin.findOne({ email });
+    if (!admin) return res.status(400).json({ msg: "Invalid Email" });
 
-  if (admin.isBlocked) return res.status(403).json({ msg: "Admin is blocked" });
+    if (admin.isBlocked)
+      return res.status(403).json({ msg: "Admin is blocked" });
 
-  const isMatch = await bcrypt.compare(password, admin.password);
-  if (!isMatch) return res.status(400).json({ msg: "Invalid password" });
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid password" });
 
-  generateToken(admin._id, res);
-  res.status(200).json({
-    
-    _id: admin._id,
-    fullname: admin.fullname,
-    email: admin.email,
-    role: admin.role,
-    uniqueId: admin.uniqueId
-  });
-} catch (error) {
-  console.error("Login error:", error.message);
-  res.status(500).json({ msg: "Internal server error" });
-}
+    generateToken(admin._id, res);
+    res.status(200).json({
+      _id: admin._id,
+      fullname: admin.fullname,
+      email: admin.email,
+      role: admin.role,
+      uniqueId: admin.uniqueId,
+    });
+  } catch (error) {
+    console.error("Login error:", error.message);
+    res.status(500).json({ msg: "Internal server error" });
+  }
 };
 
-
-  const checkAuth = (req, res) => {
-    try {
-      res.status(200).json({
-        _id: req.admin._id,
-        fullname: req.admin.fullname,
-        email: req.admin.email,
-        role: req.admin.role,
-        uniqueId: req.admin.uniqueId
-      });
-    } catch (error) {
-      console.log("error from checkAuth", error.message);
-      res.status(500).json({ msg: error.message }); 
-    }
-  };
+const checkAuth = (req, res) => {
+  try {
+    res.status(200).json({
+      _id: req.admin._id,
+      fullname: req.admin.fullname,
+      email: req.admin.email,
+      role: req.admin.role,
+      uniqueId: req.admin.uniqueId,
+    });
+  } catch (error) {
+    console.log("error from checkAuth", error.message);
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 // Get all admins
-const getalladmins = async (req, res) => {  
+const getalladmins = async (req, res) => {
   try {
-    const admins = await Admin.find({role: {$ne: "superadmin"}});
+    const admins = await Admin.find({ role: { $ne: "superadmin" } });
     res.status(200).json(admins);
   } catch (error) {
     console.log("error from getalladmins", error.message);
@@ -144,7 +141,7 @@ const unblockAdmin = async (req, res) => {
   }
 };
 
-const logoutAdmin = async  (req, res) => {
+const logoutAdmin = async (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
     return res.status(200).json({ msg: "Logout success" });
@@ -155,4 +152,12 @@ const logoutAdmin = async  (req, res) => {
 };
 
 // Export the functions using CommonJS
-module.exports = { addadmin,login,checkAuth,getalladmins,logoutAdmin,blockAdmin,unblockAdmin };
+module.exports = {
+  addadmin,
+  login,
+  checkAuth,
+  getalladmins,
+  logoutAdmin,
+  blockAdmin,
+  unblockAdmin,
+};
--- server/controllers/contractor.control.js
+++ server/controllers/contractor.control.js
@@ -1,72 +1,85 @@
-const contractor = require("../model/contractor.js")   
-
+const contractor = require("../model/contractor.js");
 
 const requeststepone = async (req, res) => {
-    try {
-        console.log("requeststepone");
-        
-       const data = await  contractor.find({registrationStep: 1}&&{approvalStatus: "Pending"});
-        res.status(200).json(data); 
-    } catch (error) {
-        console.log("error from requeststepone", error.message);
-        res.status(500).json({ msg: error.message }); 
-    }
+  try {
+    console.log("requeststepone");
+
+    const data = await contractor.find(
+      { registrationStep: 1 } && { approvalStatus: "Pending" }
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("error from requeststepone", error.message);
+    res.status(500).json({ msg: error.message });
+  }
 };
 const requestreject = async (req, res) => {
-    try {
-        console.log("requestreject");
-        const { id } = req.params;
-       const data = await  contractor.findByIdAndUpdate(id,{approvalStatus: "Rejected"});
-        res.status(200).json(data); 
-    } catch (error) {
-        console.log("error from requestreject :", error.message);
-        res.status(500).json({ msg: error.message }); 
-    }
+  try {
+    console.log("requestreject");
+    const { id } = req.params;
+    const data = await contractor.findByIdAndUpdate(id, {
+      approvalStatus: "Rejected",
+    });
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("error from requestreject :", error.message);
+    res.status(500).json({ msg: error.message });
+  }
 };
 
-    
-    const requestapprove = async (req, res) => {   try {
-        console.log("requestapprove");
-        const { id } = req.params;
-       const data = await  contractor.findByIdAndUpdate(id,{approvalStatus: "Approved"});
-        res.status(200).json(data); 
-    } catch (error) {
-        console.log("error from requestapprove :", error.message);
-        res.status(500).json({ msg: error.message }); 
-    }  }
-    const allcontractors = async (req, res) => {
-        try {
-            console.log("AllContractors");
-            const data = await  contractor.find({approvalStatus: "Approved"});
-            res.status(200).json(data); 
-        } catch (error) {
-            console.log("error from AllContractors :", error.message);
-            res.status(500).json({ msg: error.message }); 
-        }
-    }
+const requestapprove = async (req, res) => {
+  try {
+    console.log("requestapprove");
+    const { id } = req.params;
+    const data = await contractor.findByIdAndUpdate(id, {
+      approvalStatus: "Approved",
+    });
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("error from requestapprove :", error.message);
+    res.status(500).json({ msg: error.message });
+  }
+};
+const allcontractors = async (req, res) => {
+  try {
+    console.log("AllContractors");
+    const data = await contractor.find({ approvalStatus: "Approved" });
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("error from AllContractors :", error.message);
+    res.status(500).json({ msg: error.message });
+  }
+};
 
-    const blockcontractor = async (req, res) => {
-        try {
-            console.log("blockcontractor");
-            const { id } = req.params;
-           const data = await  contractor.findByIdAndUpdate(id,{isBlocked: true});
-            res.status(200).json(data); 
-        } catch (error) {
-            console.log("error from blockcontractor :", error.message);
-            res.status(500).json({ msg: error.message }); 
-        }
-    }
-    
-    const unblockcontractor = async (req, res) => {
-        try {
-            console.log("unblockcontractor");
-            const { id } = req.params;
-           const data = await  contractor.findByIdAndUpdate(id,{isBlocked: false});
-            res.status(200).json(data); 
-        } catch (error) {
-            console.log("error from unblockcontractor :", error.message);
-            res.status(500).json({ msg: error.message }); 
-        }
-    }
+const blockcontractor = async (req, res) => {
+  try {
+    console.log("blockcontractor");
+    const { id } = req.params;
+    const data = await contractor.findByIdAndUpdate(id, { isBlocked: true });
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("error from blockcontractor :", error.message);
+    res.status(500).json({ msg: error.message });
+  }
+};
 
-module.exports = { requeststepone, requestreject, requestapprove, allcontractors, blockcontractor, unblockcontractor };
+const unblockcontractor = async (req, res) => {
+  try {
+    console.log("unblockcontractor");
+    const { id } = req.params;
+    const data = await contractor.findByIdAndUpdate(id, { isBlocked: false });
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("error from unblockcontractor :", error.message);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+module.exports = {
+  requeststepone,
+  requestreject,
+  requestapprove,
+  allcontractors,
+  blockcontractor,
+  unblockcontractor,
+};
