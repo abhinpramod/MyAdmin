8db1d1c151a829c4c0a9791d8fb9a755cdb2cee6
Abhin prmod <abhinabhi310@gmail.com>
2025-02-15 01:17:30 +0530

    contractor block and unblock seted

--- server/routes/contractorcontrol.js
+++ server/routes/contractorcontrol.js
@@ -1,23 +1,22 @@
-const express= require('express')
+const express = require("express");
 const router = express.Router();
-const  {
-    requeststepone,
-    // requestdocuments
-    requestreject,
-    requestapprove,
-    allcontractors,
-    blockcontractor,
-    unblockcontractor
-} =require( "../controllers/contractor.control");
+const {
+  requeststepone,
+  // requestdocuments
+  requestreject,
+  requestapprove,
+  allcontractors,
+  blockcontractor,
+  unblockcontractor,
+} = require("../controllers/contractor.control");
 // const { blockAdmin } = require('../controllers/admincontrol');
 
- 
-router.get("/get-all-contractors", allcontractors)
-router.get("/requests/step-one", requeststepone)
-router.patch("/requests/reject/:id", requestreject)
-router.patch("/requests/approve/:id", requestapprove)
+router.get("/get-all-contractors", allcontractors);
+router.get("/requests/step-one", requeststepone);
+router.patch("/requests/reject/:id", requestreject);
+router.patch("/requests/approve/:id", requestapprove);
 
-router.put("/block/:id", blockcontractor)
-router.put("/unblock/:id", unblockcontractor)
+router.put("/block/:id", blockcontractor);
+router.put("/unblock/:id", unblockcontractor);
 
-module.exports = router
+module.exports = router;
