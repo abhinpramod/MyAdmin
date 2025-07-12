80b597fd3d3c9f1953d497158ec801fbb5f5a1b8
Abhin prmod <abhinabhi310@gmail.com>
2025-07-12 16:33:52 +0530

    chages in message

--- server/middleware/authmiddleware.js
+++ server/middleware/authmiddleware.js
@@ -10,7 +10,7 @@ const protectRoute = async (req, res, next) => {
     const token = req.cookies.jwta;
 
     if (!token) {
-      return res.status(401).json({ msg: "Unauthorized" });
+      return res.status(401).json({ msg: " No token found Unauthorized" });
     }
 
     const decoded = jwt.verify(token, process.env.JwT_SECRET);
