81f44c0eba25c54d0885b4c0243bc0f891f4f985
Abhin prmod <abhinabhi310@gmail.com>
2025-07-12 16:28:38 +0530

    typo change d jwt

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const generateToken = async (adminId, res) => {
-  const token = jwt.sign({ adminId }, process.env.JwT_SECRET, {
+  const token = jwt.sign({ adminId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
